e6b624ec1d8cbadd6dce9902e915056f42f488f0
rajesh <rajeshsurunga@gmail.com>
2022-04-18 11:33:42 -0400

    cha

--- app/src/main/java/com/example/myapplicationwe/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationwe/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        print("hhhhhhhhhhhhhhhhhhhhhhhhhhh")
+        print("hhhhhhhhhhh")
+        print("hhhhhhhhhhh")
     }
 }
